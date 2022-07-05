64f5fe9f03097d80b64c7e7a430c0352ff4c1467
Devin Raleigh <devinkr@gmail.com>
2022-07-05 17:09:12 -0600

    Fixed express default boilerplate

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,28 @@
+// db/connection.js
+//=============================================================================
+// Mongo Atlas Connection
+//=============================================================================
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// Create a .env file and paste your DATABASE_URL in it to connect your MongoDB Atlas database
-// Remember to restart your server if needed
+// Mongo URL and Connection
 const mongoURI = process.env.DATABASE_URL;
+const db = mongoose.connection;
 
-mongoose
-	.connect(mongoURI)
-	.then((conn) => {
-		console.log(
-			`Connected to MongoDB Atlas on ${conn.connections[0].name} database. Life is good 😎`
-		);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+// Connect to Mongo
+mongoose.connect(mongoURI);
 
+// Connection Error/Success - optional but can be helpful
+// Define callback functions for various events
+db.on('error', (err) => console.log(err.message + ' is MongoDB not running?'));
+db.on('connected', () => console.log('mongoDB connected at: ', mongoURI));
+db.on('disconnected', () => console.log('mongoDB disconnected'));
+
+// Open the Connection
+db.on('open', () => {
+	console.log('✅ mongo connection made!');
+});
+
+// now, our mongoose instance has a configured connection to our local db, in addition
+// to its model configuration and can be exported to other files
 module.exports = mongoose;
