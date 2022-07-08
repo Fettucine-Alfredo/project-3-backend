f734bec317fd54a1bd10e1176c1eb860ad9994b3
StarlightStingray <somajstingray@gmail.com>
2022-07-08 16:07:03 -0700

    Fix path casing error

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Job = require('../models/Job');
+const Job = require('../models/job');
 
 // GET (index) /api/job/
 router.get('/', (req, res, next) => {
